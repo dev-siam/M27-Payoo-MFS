134e5ac4ce5d54093e179d8a78b41f85d90af854
Dev Siam <dev.siam96@gmail.com>
2024-09-19 19:20:17 +0600

    all SET

--- js/features.js
+++ js/features.js
@@ -7,6 +7,11 @@ document
     document.getElementById("add-money-form").classList.remove("hidden");
     // hide the cash-out-form
     document.getElementById("cash-out-form").classList.add("hidden");
+
+    // Add a border to the button
+    document.getElementById("show-add-money-btn").style.background = "#68eba5";
+    document.getElementById("show-cash-out-btn").style.background = 'white';
+    
   });
 
 
@@ -19,4 +24,8 @@ document
     document.getElementById("cash-out-form").classList.remove("hidden");
     // hide the add-money-form
     document.getElementById("add-money-form").classList.add("hidden");
+
+    // Add a border to the button
+    document.getElementById("show-cash-out-btn").style.background = "#68eba5";
+    document.getElementById("show-add-money-btn").style.background = 'white';
   });
